40746349743be9917baa42770e8ab005d2124580
duniyash <livewirefour@gmail.com>
2022-04-01 15:44:16 +0530

    added modal and image capture

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+const startTimerBtn = document.querySelector('#startButton');
+const modal = document.querySelector('#modal');
+
+startTimerBtn.addEventListener('click', () => {
+    modal.showModal();
+})
+
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+
+camera_button.addEventListener('click', async function() {
+   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+	video.srcObject = stream;
+});
+
+click_button.addEventListener('click', function() {
+   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   	let image_data_url = canvas.toDataURL('image/jpeg');
+
+   	// data url of the image
+   	console.log(image_data_url);
+});
