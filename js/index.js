74a4b80a65c5ac22aa5053edc568b387a4ac974b
Malintha Nilhan Samarasinghe <86799359+MalinthaSamarasinghe@users.noreply.github.com>
2022-04-19 23:41:46 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,6 +22,31 @@ const auth = getAuth();
 const database = getDatabase(app);
 const storage = getStorage();
 const dref = ref(database);
+const user = auth.currentUser;
+
+
+    // if (user !== null) 
+    // {
+    //     // The user object has basic properties such as display name, email, etc.
+    //     // The user's ID, unique to the Firebase project. Do NOT use
+    //     // this value to authenticate with your backend server, if
+    //     // you have one. Use User.getToken() instead.
+    //     const uid = user.uid;
+    //     alert(user);
+    //     alert(uid);
+
+    //     // delbtn.addEventListener('click', DeleteData(user));
+
+    //     // const displayName = user.displayName;
+    //     // const email = user.email;
+    //     // const photoURL = user.photoURL;
+    //     // const emailVerified = user.emailVerified;
+
+    // }else
+    // {
+
+    // }
+	
 
 //error 
 //const errorMessage = document.getElementById("errorMessage");
