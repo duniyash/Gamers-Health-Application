8e07eb910b499c271d9e07fac6c8f751fd114eef
Rashith <rashith.ys@gmail.com>
2022-04-22 23:36:38 +0530

    API Modified app

--- Psycho/API/app.py
+++ Psycho/API/app.py
@@ -48,4 +48,4 @@ def process_json():
         return jsonify(Response = "False", Error = "Internal Error")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=environ.get("PORT",5000))
+    app.run(debug=True)
