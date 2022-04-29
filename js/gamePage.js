711306fc59a05cbd9523b653896ea87af31c520f
Malintha Nilhan Samarasinghe <86799359+MalinthaSamarasinghe@users.noreply.github.com>
2022-04-30 01:14:25 +0530

    Update gamePage.js

--- js/gamePage.js
+++ js/gamePage.js
@@ -1,3 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, updateEmail, updatePassword, sendPasswordResetEmail, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, deleteUser } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getDatabase, set, get, ref, child, update, remove } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJhkQhDdkTbB-Uhhqiytfx6Fm5tVjb1Cs",
+    authDomain: "physico-64e55.firebaseapp.com",
+    databaseURL: "https://physico-64e55-default-rtdb.firebaseio.com",
+    projectId: "physico-64e55",
+    storageBucket: "physico-64e55.appspot.com",
+    messagingSenderId: "919838418395",
+    appId: "1:919838418395:web:4c3d8f7a3af1b0219ff3fe",
+    measurementId: "G-P2KKMSZWSK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase(app);
+const storage = getStorage();
+const dref = ref(database);
+
 const startTimerBtn = document.querySelector("#startButton");
 const stopTimerBtn = document.querySelector("#stopButton");
 const closeModalBtn = document.querySelector("#close-modal");
@@ -123,6 +148,18 @@ stopTimerBtn.addEventListener('click', () => {
   var timePlayed = min + (sec/60);
 
   //send time played to firebase here
+  var userUid = (getAuth().currentUser).uid;
+  update(ref(database, 'dashboard/' + userUid),{
+    totalHoursPlayed: timePlayed,
+  })
+  .then(()=>{
+      console.log("Data updated successfully.");
+      // showErrorMessage("Data updated successfully.");
+      // location.reload();
+  })
+  .catch((error)=>{
+      console.log(error);
+  });
 
   stopTimerBtn.style.display = 'none';
   startTimerBtn.style.display = 'block';
