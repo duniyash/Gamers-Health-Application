40617d428f6f21c443d5295fc888ba27dbddf3a9
Malintha Nilhan Samarasinghe <86799359+MalinthaSamarasinghe@users.noreply.github.com>
2022-05-01 19:10:35 +0530

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -135,40 +135,77 @@ function getplayerWeekData(){
             // alert(snapshot.val());
 
             // References
-            var mondayDB = snapshot.val().mondayhoursPlayedValorant;
-            var tuesdayDB = snapshot.val().tuesdayhoursPlayedValorant;
-            var wednesdayDB = snapshot.val().wednesdayhoursPlayedValorant;
-            var thursdayDB = snapshot.val().thursdayhoursPlayedValorant;
-            var fridayDB = snapshot.val().fridayhoursPlayedValorant;
-            var saturdayDB = snapshot.val().saturdayhoursPlayedValorant;
-            var sundayDB = snapshot.val().sundayhoursPlayedValorant;
-            
-            var test = [mondayDB, tuesdayDB, wednesdayDB, thursdayDB, fridayDB, saturdayDB, sundayDB];
+            var mondayValorantDB = snapshot.val().mondayhoursPlayedValorant;
+            var tuesdayValorantDB = snapshot.val().tuesdayhoursPlayedValorant;
+            var wednesdayValorantDB = snapshot.val().wednesdayhoursPlayedValorant;
+            var thursdayValorantDB = snapshot.val().thursdayhoursPlayedValorant;
+            var fridayValorantDB = snapshot.val().fridayhoursPlayedValorant;
+            var saturdayValorantDB = snapshot.val().saturdayhoursPlayedValorant;
+            var sundayValorantDB = snapshot.val().sundayhoursPlayedValorant;
 
-            // Chart 01
+            var userUid = (getAuth().currentUser).uid;
+            get(child(dref, 'apex/' + userUid))
+            .then((snapshot)=>{
+                if(snapshot.exists()){
+                    
+                    // References
+                    var mondayApexDB = snapshot.val().mondayhoursPlayedApex;
+                    var tuesdayApexDB = snapshot.val().tuesdayhoursPlayedApex;
+                    var wednesdayApexDB = snapshot.val().wednesdayhoursPlayedApex;
+                    var thursdayApexDB = snapshot.val().thursdayhoursPlayedApex;
+                    var fridayApexDB = snapshot.val().fridayhoursPlayedApex;
+                    var saturdayApexDB = snapshot.val().saturdayhoursPlayedApex;
+                    var sundayApexDB = snapshot.val().sundayhoursPlayedApex;
+        
+                    var test = [mondayValorantDB + mondayApexDB,
+                                tuesdayValorantDB + tuesdayApexDB,
+                                wednesdayValorantDB + wednesdayApexDB,
+                                thursdayValorantDB + thursdayApexDB,
+                                fridayValorantDB + fridayApexDB,
+                                saturdayValorantDB + saturdayApexDB,
+                                sundayValorantDB + sundayApexDB];
 
-            var ctx = document.getElementById('lineChart').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: ['Mon', 'Tue', 'wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                    datasets: [{
-                        label: 'Game Play in Minutes per day',
-                        data: test,
-                        backgroundColor: [
-                            '#132119'
+                    // Chart 01
+                    var ctx = document.getElementById('lineChart').getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'line',
+                        data: {
+                            labels: ['Mon', 'Tue', 'wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                            datasets: [{
+                                label: 'Game Play in Minutes per day',
+                                data: test,
+                                backgroundColor: [
+                                    '#132119'
 
-                        ],
-                        borderColor: '#389b29',
+                                ],
+                                borderColor: '#389b29',
 
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio :false
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio :false
+                        }
+                    });
+
                 }
+                else
+                {
+                    alert("No data found! ==> Line 69");
+                }
+            })
+            .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode);
+                    console.log(errorMessage);
             });
+
+            
+            
+
+            
 
         }
         else
@@ -195,44 +232,66 @@ function getgamesChartData(){
     get(child(dref, 'valorant/' + userUid))
     .then((snapshot)=>{
         if(snapshot.exists()){
-            
-            // alert("wada na line 454");
-            // alert(snapshot.val());
 
             // References
             var valorantDB = snapshot.val().totalHoursPlayedValorant;
 
+            var userUid = (getAuth().currentUser).uid;
+            get(child(dref, 'apex/' + userUid))
+            .then((snapshot)=>{
+                if(snapshot.exists()){
+                    
+                    // References
+                    var apexDB = snapshot.val().totalHoursPlayedApex;
+        
+                    var test = [valorantDB, apexDB];
 
-            var test = [valorantDB, 0];
+                    // Chart 03
+                    var ctx = document.getElementById('doughnut1').getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'doughnut',
+                        data: {
+                            labels: ['Valorant', 'Apex'],
 
-            // Chart 03
+                            datasets: [{
+                                label: 'games',
+                                data: test,
+                                backgroundColor: [
+                                    'rgba(133, 43, 16)',
+                                    'rgba(54, 162, 235, 1)'
+                                ],
+                                borderColor: [
+                                    'rgba(133, 43, 16)',
+                                    'rgba(54, 162, 235, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
 
-            var ctx = document.getElementById('doughnut1').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: ['Valorant', 'Apex'],
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio :false
+                        }
+                    });
+                    
 
-                    datasets: [{
-                        label: 'games',
-                        data: test,
-                        backgroundColor: [
-                            'rgba(133, 43, 16)',
-                            'rgba(54, 162, 235, 1)'
-                        ],
-                        borderColor: [
-                            'rgba(133, 43, 16)',
-                            'rgba(54, 162, 235, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio :false
                 }
+                else
+                {
+                    alert("No data found! ==> Line 69");
+                }
+            })
+            .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode);
+                    console.log(errorMessage);
             });
+
+            
+
+
+            
 
         }
         else
